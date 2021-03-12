bd29cdbfac31d4f30f98ebd01ddcfbb1cd1c66fc
Антон Молчанов <antonmolchanov97@gmail.com>
2021-03-12 15:36:17 +0200

    Coded HTML & CSS for all types of screen resolution. Added the option to close the drop-down menu by clicking on any area

--- src/01_hero/hero.js
+++ src/01_hero/hero.js
@@ -1,7 +1,13 @@
 const navBurger = document.querySelector(".hero__burger");
 const navList = document.querySelector(".burger-menu");
 navBurger.addEventListener("click", event =>{
-    // document.body.classList.toggle("active")
     navBurger.classList.toggle("active");
     navList.classList.toggle("active");
 });
+
+document.body.addEventListener('click', function (event){
+    if (!(event.target.classList.contains("active"))){
+        navBurger.classList.remove("active");
+        navList.classList.remove("active");
+    }
+})
