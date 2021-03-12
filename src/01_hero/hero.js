09f87d730662dd065c2ab61c07df3ae170404016
Антон Молчанов <antonmolchanov97@gmail.com>
2021-03-12 09:53:11 +0200

    Coded the hero section for the tablet resolution

--- src/01_hero/hero.js
+++ src/01_hero/hero.js
@@ -1,6 +1,7 @@
-$(document).ready(function() {
-    $('.hero__burger').click(function(event) {
-        $('.burger-menu, .hero__burger').toggleClass('active');
-        // $('body').toggleClass('lock');
-    });
+const navBurger = document.querySelector(".hero__burger");
+const navList = document.querySelector(".burger-menu");
+navBurger.addEventListener("click", event =>{
+    // document.body.classList.toggle("active")
+    navBurger.classList.toggle("active");
+    navList.classList.toggle("active");
 });
